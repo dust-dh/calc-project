6e5169946cbdb1ee24db732799030a6063e458a1
chenai <2329876094@qq.com>
2026-05-21 15:27:37 +0800

    feat: 实现减法功能

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     int x = 10, y = 5;
     printf("%d + %d = %d\n", x, y, add(x, y));
+    printf("%d - %d = %d\n", x, y, subtract(x, y));
     return 0;
 }
