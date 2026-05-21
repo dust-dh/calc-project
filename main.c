f40de4b314d3d8998aff492cb97b03c259805382
chenai <2329876094@qq.com>
2026-05-21 15:56:08 +0800

    feat: 实现乘法功能

--- main.c
+++ main.c
@@ -8,9 +8,14 @@ int subtract(int a, int b) {
     return a - b;
 }
 
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     int x = 10, y = 5;
     printf("%d + %d = %d\n", x, y, add(x, y));
     printf("%d - %d = %d\n", x, y, subtract(x, y));
+    printf("%d * %d = %d\n", x, y, multiply(x, y));
     return 0;
 }
